5c53d4db71c9474f2129a58096c3a383e8eefcd9
drbloo <113809641+plo-0318@users.noreply.github.com>
2022-10-17 00:07:48 -0700

    added logout cookie option

--- controllers/authController.js
+++ controllers/authController.js
@@ -90,10 +90,21 @@ exports.signup = catchAsync(async (req, res, next) => {
 });
 
 exports.logout = (req, res) => {
-  res.cookie('jwt', 'loggedout', {
-    expiresIn: Date.now() + 10 * 1000,
+  const cookieOptions = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+    ),
     httpOnly: true,
-  });
+    sameSite: 'lax',
+    secure: false,
+  };
+
+  // For heroku
+  if (req.secure || req.headers['x-forwarded-proto'] === 'https') {
+    cookieOptions.secure = true;
+    coo
+
+  res.cookie('jwt', 'loggedout', cookieOptions);
 
   res.status(200).json({
     status: 'success',
