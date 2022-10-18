7f0e72c7148994ae368760fd341e87dcece171ea
drbloo <113809641+plo-0318@users.noreply.github.com>
2022-10-17 17:09:47 -0700

    changed cookie option

--- controllers/authController.js
+++ controllers/authController.js
@@ -42,8 +42,8 @@ createSendToken = (user, statusCode, req, res) => {
   // For safari
   if (req.secure || req.headers['x-forwarded-proto'] === 'https') {
     cookieOptions.secure = true;
-    cookieOptions.domain = '.tuffytalk.herokuapp.com';
-    cookieOptions.sameSite = 'lax';
+    cookieOptions.domain = 'tuffytalk.herokuapp.com';
+    cookieOptions.sameSite = 'none';
   }
 
   res.cookie('jwt', token, cookieOptions);
@@ -109,8 +109,8 @@ exports.logout = (req, res) => {
   // For safari
   if (req.secure || req.headers['x-forwarded-proto'] === 'https') {
     cookieOptions.secure = true;
-    cookieOptions.domain = '.tuffytalk.herokuapp.com';
-    cookieOptions.sameSite = 'lax';
+    cookieOptions.domain = 'tuffytalk.herokuapp.com';
+    cookieOptions.sameSite = 'none';
   }
 
   res.cookie('jwt', 'loggedout', cookieOptions);
